1231add0d5e9f4f72eff16dcd3127264c1584bbc
HebibAskerli <hebibeskerli@gmail.com>
2022-10-01 16:48:09 +0400

    Duzeltildi

--- firstLessonHomework/src/variables.java
+++ firstLessonHomework/src/variables.java
@@ -1,7 +1,8 @@
 public class variables {
     public static void main(String[] args) throws Exception {
         int ogrenciSayisi = 10;
-        
+
+        System.out.println("Ogrenci sayisi :" + ogrenciSayisi);
         System.out.println("Ogrenci sayisi :" + ogrenciSayisi);
         System.out.println("Ogrenci sayisi :" + ogrenciSayisi);
         System.out.println("Ogrenci sayisi :" + ogrenciSayisi);
