9a7a36e30e55e8f76a2a7dfd8e3a221343b3f3ba
HebibAskerli <hebibeskerli@gmail.com>
2022-10-09 01:34:36 +0400

    Understanding Methods in Java

---
+++ secondLessonHomework/src/UnderstandingMethods.java
@@ -0,0 +1,29 @@
+public class UnderstandingMethods {
+    public static void main(String[] args) {
+        findNumber();
+    }
+
+    public static void findNumber(){
+        int[] numbers = new int[]{1,2,5,6,8,0};
+        int numberToBeFound = 6;
+        boolean state = false;
+
+        for(int number : numbers){
+            if(number == numberToBeFound){
+                state = true;
+                break;
+            }
+        }
+        if(state){
+            giveMessage("Number exists: " + numberToBeFound);
+        }else{
+            System.out.println("Number doesnt exist." + numberToBeFound);
+        }
+    }
+
+    public static void giveMessage(String message){
+        System.out.println(message);
+    }
+
+
+}
