c9fe0f763b223e9840448c6a667d71f0dd5a69aa
HebibAskerli <hebibeskerli@gmail.com>
2022-10-09 02:10:09 +0400

    reCapDemo

---
+++ secondLessonHomework/src/reCapDemo_Classes.java
@@ -0,0 +1,8 @@
+public class reCapDemo_Classes {
+    public static void main(String[] args) {
+
+        reCapDemo_Methods reCapDemoMethods = new reCapDemo_Methods();
+
+        System.out.println(reCapDemoMethods.topla(3, 4));
+    }
+}
---
+++ secondLessonHomework/src/reCapDemo_Methods.java
@@ -0,0 +1,17 @@
+public class reCapDemo_Methods  {
+    public int topla(int sayi1,int sayi2){
+        return sayi1+sayi2;
+    }
+
+    public int cikma(int sayi1,int sayi2){
+        return sayi1-sayi2;
+    }
+
+    public int vurma(int sayi1,int sayi2){
+        return sayi1*sayi2;
+    }
+
+    public int bolme(int sayi1,int sayi2){
+        return sayi1/sayi2;
+    }
+}
