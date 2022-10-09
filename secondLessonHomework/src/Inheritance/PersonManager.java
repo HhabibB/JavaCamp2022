72774e560fa5cd380618a6df7ed51afed7bc4645
HebibAskerli <hebibeskerli@gmail.com>
2022-10-09 16:23:23 +0400

    Inheritance

---
+++ secondLessonHomework/src/Inheritance/PersonManager.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class PersonManager {
+
+    public void List(){
+        System.out.println("Musteri listelendi.");
+    }
+    public void Add(){
+        System.out.println("Musteri eklendi.");
+    }
+}
