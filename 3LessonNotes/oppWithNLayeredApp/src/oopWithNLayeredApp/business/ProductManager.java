af27e50e1d07de38dac0ebffc100cae5bf01da88
HebibAskerli <hebibeskerli@gmail.com>
2022-10-11 14:43:18 +0400

    oop with N layered app

--- 3LessonNotes/oppWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ 3LessonNotes/oppWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,15 +1,31 @@
 package oopWithNLayeredApp.business;
 
-import oopWithNLayeredApp.dataAccess.JdbcProductDao;
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.dataAccess.ProductDao;
 import oopWithNLayeredApp.entities.Product;
 
+import java.util.List;
+
 public class ProductManager {
+    private ProductDao productDao;
+    private Logger[] loggers;
+
+    public ProductManager(ProductDao productDao,Logger[] loggers) {
+        this.productDao = productDao;
+        this.loggers = loggers;
+    }
+
     public void add(Product product) throws Exception {
         //business rules
-        if(product.getUnitPrice() < 10){
+        if (product.getUnitPrice() < 10) {
             throw new Exception("Urun Fiyati 10-dan kucuk olamaz");
         }
-        JdbcProductDao productDao = new JdbcProductDao();
         productDao.add(product);
+
+        for (Logger logger: loggers) { //[db,mail]
+            logger.log(product.getName());
+        }
+
     }
+
 }
--- 3LessonNotes/oppWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
+++ 3LessonNotes/oppWithNLayeredApp/src/oopWithNLayeredApp/dataAccess/HibernateProductDao.java
@@ -2,8 +2,9 @@ package oopWithNLayeredApp.dataAccess;
 
 import oopWithNLayeredApp.entities.Product;
 
-public class JdbcProductDao implements ProductDao {
+public class HibernateProductDao implements ProductDao {
     public void add(Product product){
-        System.out.println("JDBC ile veritabanina eklendi.");
+        System.out.println("Hibernate ile veritabanina eklendi.");
     }
+
 }
